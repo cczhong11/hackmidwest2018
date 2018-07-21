3fdaf8024c69b45ccf8c8165ff92d5edb0a638fe
Ubuntu <ubuntu@ip-172-31-81-46.ec2.internal>
2018-07-21 23:43:13 +0000

    query all

--- routes/api.js
+++ routes/api.js
@@ -1,74 +1,75 @@
-	var express = require('express');
-	var router = express.Router();
+var express = require('express');
+var router = express.Router();
 
-	/* GET users listing. */
-	var AWS = require("aws-sdk");
+/* GET users listing. */
+var AWS = require("aws-sdk");
 
-	AWS.config.update({
-	    region: "us-east-1"
-	});
+AWS.config.update({
+    region: "us-east-1"
+});
 
-	var docClient = new AWS.DynamoDB.DocumentClient();
-	var db = new AWS.DynamoDB();
-	var table = "hackathon";
+var docClient = new AWS.DynamoDB.DocumentClient();
+var db = new AWS.DynamoDB();
+var table = "hackathon";
 
 
-	router.post('/', function (req, res, next) {
-	    console.log(JSON.stringify(req.query))
-	    if (req.query.op === "create") {
-		var macaddress = req.query.addr
-		// check whether it is in the table first
-		var get_params = {
-		    TableName: table,
-		    Key: {
-			'macaddress': {
-			    "S": macaddress
-			},
-		    }
-		};
-		var flag = 0;
-		db.getItem(get_params, function (err, data) {
-		    if (err) {
-			console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-		    } else {
-			console.log("Added item3 :", JSON.stringify(data));
-			if ("Item" in data && data.Item.length !== 0) {
-			    res.json({
-				"status": "again"
-			    })
-			} else {
-			    var uuid = Math.random().toString(16).substring(2, 7);
-			    var params = {
-				TableName: table,
-				Item: {
-				    "macaddress": macaddress,
-				    "phone": "not set",
-				    "timestamp": new Date().toISOString(),
-				    "uuid": uuid,
-				    "info": {
-					"foursqure": "none",
-					"rating": 0
-				    }
-				}
-			    }
-			    docClient.put(params, function (err, data) {
-				if (err) {
-				    console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-				} else {
-				    console.log("Added item:", JSON.stringify(data, null, 2));
-				}
-			    });
-			    res.json({
-				"status": uuid
-			    })
-			}
+router.post('/', function (req, res, next) {
+    console.log(JSON.stringify(req.query))
+    if (req.query.op === "create") {
+        var macaddress = req.query.addr
+        // check whether it is in the table first
+        var get_params = {
+            TableName: table,
+            Key: {
+                'macaddress': {
+                    "S":  macaddress
+                },
+            }
+        };
+        var flag = 0;
+        db.getItem(get_params, function (err, data) {
+            if (err) {
+                console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+            } else {
+                console.log("Added item3 :", JSON.stringify(data));
+                if ("Item" in data && data.Item.length !== 0) {
+                    res.json({
+                        "status": "again"
+                    })
+                } else {
+                    var uuid = Math.random().toString(16).substring(2, 7);
+                    var params = {
+                        TableName: table,
+                        Item: {
+                            "macaddress": macaddress,
+                            "phone": "not set",
+                            "timestamp": new Date().toISOString(),
+                            "uuid": uuid,
+                            "info": {
+                                "foursqure": "none",
+                                "rating": 0
+                            }
+                        }
+                    }
+                    docClient.put(params, function (err, data) {
+                        if (err) {
+                            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+                        } else {
+                            console.log("Added item:", JSON.stringify(data, null, 2));
+                        }
+                    });
+                    res.json({
+                        "status": uuid
+                    })
+                }
 
-		    }
-		});
-
-	    }else if(req.query.op==="postarray"){
-		console.log(req.body)
+            }
+        });
+    }else if(req.query.op==="postarray"){
+        console.log(req.body);
+        res.json({"res":"success"})
     }
+
 });
 router.get('/', function (req, res, next) {
     if (req.query.op === "get") {
--- routes/phone.js
+++ routes/phone.js
@@ -12,10 +12,19 @@ var docClient = new AWS.DynamoDB.DocumentClient();
 var table = "hackathon";
 
 
-router.post('/', function (req, res, next) {
+router.get('/', function (req, res, next) {
     console.log(JSON.stringify(req.query))
-    if (req.query.op === "create") {
-
+    if (req.query.op === "getall") {
+        var table = "hackathon"
+        var scan_params = {
+            TableName: table
+        };
+    
+        docClient.scan(scan_params, function (err, data) {
+            if (err) {
+                console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+            } else {
+                console.log(data.Items);}});
     }
 });
 
