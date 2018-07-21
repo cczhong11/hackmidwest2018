cb9cdad9a4e23a338c4930ab8218c070d7e22eaa
Ram G Athreya <Ram.Athreya@walmart.com>
2018-07-21 12:36:30 -0500

    Basic Twilio Integration

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -37,5 +39,17 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+var twilio = require('twilio');
+var accountSid = 'AC5d3e038364d1c3628c4a56a1585ade0c';
+var authToken = '047877cc86d2dc3b072e4beb4b57606a';
+var client = new twilio(accountSid, authToken);
+
+// client.messages.create({
+//     body: 'Hello from Node',
+//     to: '+14802779750',  // Text this number
+//     from: '+14804627562' // From a valid Twilio number
+// })
+//     .then((message) => console.log(message.sid));
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post("/sms", (req, res) => {
+    const twiml = new MessagingResponse();
+    twiml.message('The Robots are coming! Head for the hills!');
+
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+});
+
 
 module.exports = router;
